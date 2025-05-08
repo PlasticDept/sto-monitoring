19e0a8f09591ade30bb33b5dd48c1bcce9255382
PlasticDept <plastic.npi.lc3a@outlook.com>
2025-05-08 14:06:59 +0700

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,16 +1,3 @@
-// Inisialisasi Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyC9snZ35bOtHBVl59LdAUtrwXkL_6z8xJk",
-  authDomain: "sto-monitoring.firebaseapp.com",
-  projectId: "sto-monitoring",
-  storageBucket: "sto-monitoring.firebasestorage.app",
-  messagingSenderId: "180070053972",
-  appId: "1:180070053972:web:1a23b7128e98d8332255c1",
-  databaseURL: "https://sto-monitoring-default-rtdb.asia-southeast1.firebasedatabase.app/"
-};
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
-
 // Fungsi untuk parsing CSV ke array objek
 function parseCSV(csvText) {
   const lines = csvText.trim().split("\n");
