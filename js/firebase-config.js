cc3971c47f052539e662cc26e10cdbd13e61a7d3
PlasticDept <plastic.npi.lc3a@outlook.com>
2025-05-08 13:26:39 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBYdbo6du0u3ZxT53lFEXpNccPwTu8czN4",
-    authDomain: "incoming-schedule-monitoring.firebaseapp.com",
-    databaseURL: "https://incoming-schedule-monitoring-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "incoming-schedule-monitoring",
-    storageBucket: "incoming-schedule-monitoring.firebasestorage.app",
-    messagingSenderId: "460704037681",
-    appId: "1:460704037681:web:2fdbf601beaa8817130e10"
+    apiKey: "AIzaSyC9snZ35bOtHBVl59LdAUtrwXkL_6z8xJk",
+    authDomain: "sto-monitoring.firebaseapp.com",
+    projectId: "sto-monitoring",
+    storageBucket: "sto-monitoring.firebasestorage.app",
+    messagingSenderId: "180070053972",
+    appId: "1:180070053972:web:1a23b7128e98d8332255c1"
+    databaseURL: "https://sto-monitoring-default-rtdb.asia-southeast1.firebasedatabase.app/"
   };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
