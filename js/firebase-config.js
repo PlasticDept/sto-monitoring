5dea1b8f472e6aaa4e0a9925e073715fc421e460
PlasticDept <plastic.npi.lc3a@outlook.com>
2025-05-08 13:52:25 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
     projectId: "sto-monitoring",
     storageBucket: "sto-monitoring.firebasestorage.app",
     messagingSenderId: "180070053972",
-    appId: "1:180070053972:web:1a23b7128e98d8332255c1"
+    appId: "1:180070053972:web:1a23b7128e98d8332255c1",
     databaseURL: "https://sto-monitoring-default-rtdb.asia-southeast1.firebasedatabase.app/"
   };
 firebase.initializeApp(firebaseConfig);
